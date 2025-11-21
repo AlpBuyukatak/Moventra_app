63b702a0f7d24416959421cf96a5f73e2bad1247
Alp <111985901+AlpBuyukatak@users.noreply.github.com>
2025-11-21 01:21:19 +0100

    aaa

--- moventra/backend/prisma/migrations/20251120134207_init/migration.sql
+++ moventra/backend/prisma/migrations/20251120134207_init/migration.sql
@@ -10,7 +10,7 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable // Duzgun biseyaa
+-- CreateTable // Duzgun biseyaaaa
 CREATE TABLE "Hobby" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
