4c3acdcf264c203094856ff2b1b863d32d93207b
uygar061 <elmali.uygar@gmail.com>
2025-11-21 03:09:31 +0300

    denemeuygar

--- moventra/backend/prisma/migrations/20251120134207_init/migration.sql
+++ moventra/backend/prisma/migrations/20251120134207_init/migration.sql
@@ -10,7 +10,7 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable // dfgddhfdhdfgfdhjhbvgc
+-- CreateTable // dfgddhfdhdfgfdhjhbvgcdsgfshdhgdj
 CREATE TABLE "Hobby" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
